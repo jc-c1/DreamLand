27022059805a5d56a00697cf65956ca80765d7b0
jc-c1 <joannec.chang1@gmail.com>
2024-03-10 00:40:38 -0800

    route login screen with details

--- components/navBar/Bot/navBarBot.js
+++ components/navBar/Bot/navBarBot.js
@@ -1,13 +1,26 @@
 import * as React from 'react';
 import { NavigationContainer} from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import navBarTop from '../Top/navBarTop';
 import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 import UserProfile from '../../user/userProfile';
 import StoryScreen from '../../../pages/StoryScreen';
+import StoryDetailScreen from '../../../pages/StoryDetailScreen'; 
 
 const Tab = createBottomTabNavigator();
+
+const MainStack = createNativeStackNavigator();
+
+function MainStackScreen() {
+  return (
+    <MainStack.Navigator>
+      <MainStack.Screen name="Story" component={StoryScreen} />
+      <MainStack.Screen name="StoryDetail" component={StoryDetailScreen} />
+    </MainStack.Navigator>
+  );
+}
 
 export const NavBarBot = () => {
     return (
@@ -18,7 +31,7 @@ export const NavBarBot = () => {
                 tabBarInactiveTintColor: "lightgrey",
             }} initialRouteName="Home">
 
-                <Tab.Screen name="Home" component={StoryScreen} options={{
+                <Tab.Screen name="Home" component={MainStackScreen} options={{
                     tabBarIcon: ({ color }) => <Ionicons name="library-sharp" size={20} color={color} />
                 }}/>
                 <Tab.Screen name="Adventure" component={navBarTop} options={{
@@ -28,7 +41,7 @@ export const NavBarBot = () => {
                     tabBarLabel: "Profile", 
                     tabBarIcon: ({ color }) => <Ionicons name="person-sharp" size={20} color={color} />}} />
 
-            </Tab.Navigator>
+            </Tab.Navigator>            
         </NavigationContainer>
     )
 }
