aae1c5e11b46d63df017d90e1bacfeef5d6d859a
sigma <rhoda312312@gmail.com>
2024-03-09 21:07:53 -0800

    nav function added

--- components/navBar/Bot/navBarBot.js
+++ components/navBar/Bot/navBarBot.js
@@ -1,6 +1,9 @@
+import * as React from 'react';
 import { NavigationContainer} from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import navBarTop from '../Top/navBarTop';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,9 +15,15 @@ export default function navBarBot() {
                 tabBarActiveTintColor: "purple",
                 tabBarInactiveTintColor: "lightgrey",
             }}>
-                {/* <Tab.Screen name="Home" component={}/>
-                <Tab.Screen name="Adventure" component={navBarTop} />
-                <Tab.Screen name="Profile" component={} options={{tabBarLabel: "Profile"}}/> */}
+                {/* <Tab.Screen name="Home" component={} options={{
+                    tabBarIcon: ({ color }) => <Ionicons name="library-sharp" size={20} color={color} />
+                }}/>
+                <Tab.Screen name="Adventure" component={navBarTop} options={{
+                    tabBarIcon: ({ color }) => <FontAwesome5 name="paint-brush" size={24} color={color} />
+                }}/>
+                <Tab.Screen name="Profile" component={} options={{
+                    tabBarLabel: "Profile", 
+                    tabBarIcon: ({ color }) => <Ionicons name="person-sharp" size={20} color={color} />}} /> */}
             </Tab.Navigator>
         </NavigationContainer>
     )
