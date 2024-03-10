4ddb789cd2ecc0e87bf1adec276604577c5f1f54
Rhoda <rhoda.yw@gmail.com>
2024-03-10 04:16:24 -0700

    fixed adventure's tab bar label

--- components/navBar/Bot/navBarBot.js
+++ components/navBar/Bot/navBarBot.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import { NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Ionicons } from '@expo/vector-icons';
 import { FontAwesome5 } from '@expo/vector-icons';
 import UserProfile from '../../user/userProfile';
 import StoryScreen from '../../../pages/StoryScreen';
-import StoryDetailScreen from '../../../pages/StoryDetailScreen'; 
-import PractiseScreen from '../../../pages/PractiseScreen'; 
+import StoryDetailScreen from '../../../pages/StoryDetailScreen';
+import PractiseScreen from '../../../pages/PractiseScreen';
 import BackImg from '../../../components/readingScreen/backImg';
 
 const Tab = createBottomTabNavigator();
@@ -28,32 +28,33 @@ const AdventureStack = createNativeStackNavigator();
 function AdventureStackScreen() {
   return (
     <AdventureStack.Navigator>
-      <AdventureStack.Screen name="Practise" component={PractiseScreen} />
+      <AdventureStack.Screen name="Practise" component={PractiseScreen} options={{ headerShown: false }} />
       <AdventureStack.Screen name="Background" component={BackImg} />
     </AdventureStack.Navigator>
   );
 }
 
 export const NavBarBot = () => {
-    return (
-        <NavigationContainer>
-            <Tab.Navigator
-            screenOptions={{
-                tabBarActiveTintColor: "purple",
-                tabBarInactiveTintColor: "lightgrey",
-            }} initialRouteName="Home">
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "purple",
+          tabBarInactiveTintColor: "lightgrey",
+        }} initialRouteName="Home">
 
-                <Tab.Screen name="Home" component={MainStackScreen} options={{
-                    tabBarIcon: ({ color }) => <Ionicons name="library-sharp" size={20} color={color} />
-                }}/>
-                <Tab.Screen name="Adventure" component={AdventureStackScreen} options={{
-                    tabBarIcon: ({ color }) => <FontAwesome5 name="paint-brush" size={24} color={color} />
-                }}/>
-                <Tab.Screen name="Profile" component={UserProfile} options={{
-                    tabBarLabel: "Profile", 
-                    tabBarIcon: ({ color }) => <Ionicons name="person-sharp" size={20} color={color} />}} />
+        <Tab.Screen name="Home" component={MainStackScreen} options={{
+          tabBarIcon: ({ color }) => <Ionicons name="library-sharp" size={20} color={color} />
+        }} />
+        <Tab.Screen name="Start Your Adventure!" component={AdventureStackScreen} options={{
+          tabBarLabel: "Adventure",
+          tabBarIcon: ({ color }) => <FontAwesome5 name="paint-brush" size={24} color={color} />
+        }} />
+        <Tab.Screen name="Profile" component={UserProfile} options={{
+          tabBarIcon: ({ color }) => <Ionicons name="person-sharp" size={20} color={color} />
+        }} />
 
-            </Tab.Navigator>            
-        </NavigationContainer>
-    )
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
