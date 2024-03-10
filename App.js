70d12b79986c88503c080a5b49a88e2b847b1113
kellychen <chenkai@students.cs.ubc.ca>
2024-03-09 22:49:47 -0800

    added storyDetail screen basic

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MainScreen from './pages/MainScreen';
 import StoryScreen from './pages/StoryScreen';
 import PractiseScreen from './pages/PractiseScreen';
+import StoryDetailScreen from './pages/StoryDetailScreen'; // You will create this component
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,7 @@ function App() {
         <Stack.Screen name="Main" component={MainScreen} />
         <Stack.Screen name="Story" component={StoryScreen} />
         <Stack.Screen name="Practise" component={PractiseScreen} />
+        <Stack.Screen name="StoryDetail" component={StoryDetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
