ed96fba6e96c609d79baefd59c2144c56669b505
sigma <rhoda312312@gmail.com>
2024-03-10 11:15:24 -0700

    changed opacity in story detail screen bg

--- pages/StoryDetailScreen.js
+++ pages/StoryDetailScreen.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, ImageBackground, ScrollView } from 'react-native';
 import { collection, query, where, getDocs } from 'firebase/firestore'; // Make sure all these are imported
-import { db } from '../config/firebase.js'; // Check this path is correct as per your project structure
-
+import { db } from '../config/firebase.js'; 
 
 function StoryDetailScreen({ route }) {
   const [storyDetails, setStoryDetails] = useState({});
-  const storyTitle = route.params.storyTitle; // Get the story title passed through navigation
+  const storyTitle = route.params.storyTitle; 
   const backgroundImage = route.params.backgroundImage;
   
   useEffect(() => {
@@ -57,15 +56,15 @@ const styles = StyleSheet.create({
   },
   bgImage: {
     flex: 1,
-    // Ensure the children components are positioned correctly
     padding: 100,
     alignItems: "center",
+    opacity: 0.9,
   },
   fullText: {
     backgroundColor: 'rgba(255,255,255,0.6)', // Semi-translucent black background
-    borderRadius: 15, // Rounded edges
-    padding: 20, // Padding for content inside the box
-    margin: -80, // Margin around the box
+    borderRadius: 15, 
+    padding: 20, 
+    margin: -80, 
     marginTop: 10,
     marginBottom: -40,
   },
